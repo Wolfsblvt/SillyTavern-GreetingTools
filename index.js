55547cfa358c732f3b4167c94b1e76bdd681c622
Wolfsblvt <wolfsblvt@gmail.com>
2026-03-21 21:43:26 +0100

    Add initialization guard to prevent duplicate extension setup
    Add initializeCalled flag to prevent init function from running multiple times. Export initialized flag to allow external code to check initialization state. Set initialized to true after setup completes.

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ import { initGreetingSelector } from './greeting-selector.js';
 export const EXTENSION_KEY = 'greeting_tools';
 export const EXTENSION_NAME = 'SillyTavern-GreetingTools';
 
+let initializeCalled = false;
+export let initialized = false;
+
 /**
  * Extension initialization
  */
 export async function init() {
+    if (initializeCalled) return;
+    initializeCalled = true;
+
     console.debug(`[${EXTENSION_NAME}] Extension loaded`);
     setupButtonIntercept();
     initGreetingSelector();
+
+    initialized = true;
 }
